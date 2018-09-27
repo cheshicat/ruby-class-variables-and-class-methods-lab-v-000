4cd283ae8df2f85114b074280083e27f84a48cdd
Allison Howard <alliemhow@gmail.com>
2018-09-27 10:59:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count +=  1
-    @@genres << self.genre
-    @@artists << self.artist
+    @@genres[genre]
+    @@artists[artist]
   end
 
   def self.count
