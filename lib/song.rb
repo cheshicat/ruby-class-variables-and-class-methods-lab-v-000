681152d8eec3ae62ac2f19ae78089e4944505317
Allison Howard <alliemhow@gmail.com>
2018-09-27 10:42:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,10 +10,15 @@ class Song
     @artist = artist
     @genre = genre
     @@count +=  1
+    @@genres << genre
   end
 
   def self.count
     @@count
+  end
+
+  def self.genres
+    @@genres.uniq
   end
 
 end
